b157c6929ea6431d56ea8dcb050c5972ced1c1a0
Vedant Jain <contact@darteweb.com>
2017-03-14 17:10:21 +0530

    first

---
+++ test1.php
@@ -0,0 +1,21 @@
+<?php
+# @Author: Vedant Jain
+# @Date:   2017-03-14T15:38:48+05:30
+# @Email:  getgimphed@gmail.com
+# @Project: test
+# @Filename: test1.php
+# @Last modified by:   Vedant Jain
+# @Last modified time: 2017-03-14T15:39:09+05:30
+# @License: private
+# @Copyright: Vedant
+
+
+/**
+ * [FunctionName description]
+ * @param string $value [description]
+ */
+
+public function FunctionName($value='')
+{
+  # code...
+}
